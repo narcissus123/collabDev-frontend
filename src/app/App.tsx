129283f5bfddb2ec7635abaae543238a92540dbf
narcissus123 <haeri.n@yahoo.com>
2024-12-03 08:43:12 -0500

    Cleaned the code.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,16 @@
 import { useEffect } from "react";
-import { useAuth } from "../context/AuthContext/AuthContext";
 import { SocketProvider } from "../context/SocketContext/SocketContext";
 import UnAuth from "./UnAuth/UnAuth";
 
 import "./App.scss";
 
 const App: React.FC = () => {
-  const { isAuthenticated } = useAuth();
-
   useEffect(() => {
     window.process = {
       ...window.process,
     };
   }, []);
-  console.log("isAuthenticated", isAuthenticated);
+
   return (
     <>
       <SocketProvider>
