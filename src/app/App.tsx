56da300a14530f7937f0447e65faca88b133f0dd
narcissus123 <haeri.n@yahoo.com>
2024-12-03 08:42:15 -0500

    Updated App to use Auth context.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,27 +1,24 @@
 import { useEffect } from "react";
-
 import { useAuth } from "../context/AuthContext/AuthContext";
 import { SocketProvider } from "../context/SocketContext/SocketContext";
-import scrollToTopUtility from "../core/utils/ScrollToTop/scrollToTopUtility";
-
-import Auth from "./Auth/Auth";
 import UnAuth from "./UnAuth/UnAuth";
 
 import "./App.scss";
 
 const App: React.FC = () => {
-  const user = useAuth();
-  scrollToTopUtility();
+  const { isAuthenticated } = useAuth();
+
   useEffect(() => {
     window.process = {
       ...window.process,
     };
   }, []);
-
+  console.log("isAuthenticated", isAuthenticated);
   return (
     <>
-      {/* <ScrollToTop /> */}
-      <SocketProvider>{user.isAdmin ? <Auth /> : <UnAuth />}</SocketProvider>
+      <SocketProvider>
+        <UnAuth />
+      </SocketProvider>
     </>
   );
 };
