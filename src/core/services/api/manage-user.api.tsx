f39507bdcdac7911b1a12a99d70e3f121b5b51a0
narcissus123 <haeri.n@yahoo.com>
2024-12-03 04:41:43 -0500

    Cleaned the code.

--- src/core/services/api/manage-user.api.tsx
+++ src/core/services/api/manage-user.api.tsx
@@ -1,26 +1,11 @@
 import { instance } from "../interceptor/Interceptor";
 
 const updateUserInfo = async (userId: string, obj: any) => {
-  try {
-    console.log("FormData entries:");
-    for (const pair of obj.entries()) {
-      console.log(pair[0], pair[1]);
-    }
-    const response = await instance.put(`api/v1/user/${userId}`, obj);
-    if (response.status === 200) {
-      return response.data;
-    }
-  } catch (error: any) {
-    console.error("Something went wrong:", error);
-    throw new Error(
-      error.response.data.message ||
-        "Failed to update profile information. Please try again later."
-    );
-  }
+  const response = await instance.put(`api/v1/user/${userId}`, obj);
+  return response.data;
 };
 
 export const getUserById = async (studentId: string) => {
-  //await delay(2000);
   return await instance.get(`api/v1/user/${studentId}`);
 };
 
