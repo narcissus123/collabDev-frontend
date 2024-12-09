203bd2a3eb51b29dff3b298e73929286935bc178
narcissus123 <haeri.n@yahoo.com>
2024-12-08 19:55:08 -0500

    Cleaned the code.

--- src/core/utils/ImageUtils/imageUtils.ts
+++ src/core/utils/ImageUtils/imageUtils.ts
@@ -1,13 +1,8 @@
 export const getImageUrl = (path: string) => {
   // If it's a common asset, always use prod bucket
   if (path.startsWith("common/")) {
-    console.log(
-      "up",
-      `https://collabdev-prod-storage-2024.s3.us-east-2.amazonaws.com/${path}`
-    );
     return `https://collabdev-prod-storage-2024.s3.us-east-2.amazonaws.com/${path}`;
   }
-  console.log("down", `${process.env.REACT_APP_S3_BUCKET_URL}/${path}`);
   // Otherwise use environment-specific bucket
   return `${process.env.REACT_APP_S3_BUCKET_URL}/${path}`;
 };
